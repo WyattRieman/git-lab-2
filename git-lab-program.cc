d0ab0abcf3b30a5128e8414da8f4826d7b91469c
WyattRieman <wr955724@ohio.edu>
2025-09-09 16:11:48 -0400

    Commit again

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Wyatt Rieman
+ *        Date: 09/02/2025
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    /*add code*/
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
